120f28be6178c6d9217ed10aa400df472770d0dd
prototype <3489849684@qq.com>
2025-07-11 12:38:34 +0800

    添加window组件的样式

--- src/containers/Window.tsx
+++ src/containers/Window.tsx
@@ -11,10 +11,10 @@ export default function Window(props: {children: ReactNode, title: string}){
           <label className="title">{props.title}</label>
         </div>
         <div className="right">
-          <button className="min-btn" onClick={() => appWindow.minimize()}>
+          <button className="min-btn" onClick={() => appWindow.minimize()} data-pb>
             <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M2 3h20v18H2V3zm18 16V7H4v12h16zM8 12h8v2H8v-2z" fill="currentColor"/> </svg>
           </button>
-          <button className="close-btn" onClick={() => appWindow.close()}>
+          <button className="close-btn" onClick={() => appWindow.close()} data-pb>
             <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M2 3h20v18H2V3zm18 16V7H4v12h16zM9 10h2v2H9v-2zm4 2h-2v2H9v2h2v-2h2v2h2v-2h-2v-2zm0 0v-2h2v2h-2z" fill="currentColor"/> </svg>
           </button>
         </div>
